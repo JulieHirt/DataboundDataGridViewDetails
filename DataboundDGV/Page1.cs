2c1ab537a12b1a71ca4d0d21efca0f0dcf3adaf2
JulieBodette <jr3python@gmail.com>
2023-07-26 10:51:17 -0400

    Update Page1.cs

--- DataboundDGV/Page1.cs
+++ DataboundDGV/Page1.cs
@@ -15,11 +15,12 @@ namespace DataboundDGV
 		List<Company> companyList = new List<Company>();
 		private void Page1_Load(object sender, System.EventArgs e)
 		{
-			dataGridView1.DataSource = peopleList;
+			
 			peopleList.Add(new Person() { Name = "John", Company = "ITG", ID = 1 });
 			peopleList.Add(new Person() { Name = "Julie", Company = "ITG", ID = 2 });
 			peopleList.Add(new Person() { Name = "Tom", Company = "Google", ID = 3 });
 			peopleList.Add(new Person() { Name = "Susan", Company = "Google", ID = 4 });
+			dataGridView1.DataSource = peopleList;
 		}
 	}
 }
