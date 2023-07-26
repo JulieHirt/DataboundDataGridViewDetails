3f9c7a38decbd49568f477df02dc1fad1249eb08
JulieBodette <jr3python@gmail.com>
2023-07-26 10:58:54 -0400

    Data binding done correctly
    bindinglist, not list

--- DataboundDGV/Page1.Designer.cs
+++ DataboundDGV/Page1.Designer.cs
@@ -28,16 +28,50 @@
 		/// </summary>
 		private void InitializeComponent()
 		{
+			this.components = new System.ComponentModel.Container();
 			this.dataGridView1 = new Wisej.Web.DataGridView();
+			this.personBindingSource = new Wisej.Web.BindingSource(this.components);
+			this.colName = new Wisej.Web.DataGridViewTextBoxColumn();
+			this.colCompany = new Wisej.Web.DataGridViewTextBoxColumn();
+			this.colID = new Wisej.Web.DataGridViewTextBoxColumn();
 			((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+			((System.ComponentModel.ISupportInitialize)(this.personBindingSource)).BeginInit();
 			this.SuspendLayout();
 			// 
 			// dataGridView1
 			// 
+			this.dataGridView1.AutoGenerateColumns = false;
+			this.dataGridView1.Columns.AddRange(new Wisej.Web.DataGridViewColumn[] {
+            this.colName,
+            this.colCompany,
+            this.colID});
+			this.dataGridView1.DataSource = this.personBindingSource;
 			this.dataGridView1.Location = new System.Drawing.Point(318, 95);
 			this.dataGridView1.Name = "dataGridView1";
 			this.dataGridView1.Size = new System.Drawing.Size(569, 370);
 			this.dataGridView1.TabIndex = 0;
+			// 
+			// personBindingSource
+			// 
+			this.personBindingSource.DataSource = typeof(DataboundDGV.Person);
+			// 
+			// colName
+			// 
+			this.colName.DataPropertyName = "Name";
+			this.colName.HeaderText = "Name";
+			this.colName.Name = "colName";
+			// 
+			// colCompany
+			// 
+			this.colCompany.DataPropertyName = "Company";
+			this.colCompany.HeaderText = "Company";
+			this.colCompany.Name = "colCompany";
+			// 
+			// colID
+			// 
+			this.colID.DataPropertyName = "ID";
+			this.colID.HeaderText = "ID";
+			this.colID.Name = "colID";
 			// 
 			// Page1
 			// 
@@ -45,9 +79,10 @@
 			this.AutoScaleMode = Wisej.Web.AutoScaleMode.Font;
 			this.Controls.Add(this.dataGridView1);
 			this.Name = "Page1";
-			this.Size = new System.Drawing.Size(1549, 561);
+			this.Size = new System.Drawing.Size(1524, 478);
 			this.Load += new System.EventHandler(this.Page1_Load);
 			((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+			((System.ComponentModel.ISupportInitialize)(this.personBindingSource)).EndInit();
 			this.ResumeLayout(false);
 
 		}
@@ -55,6 +90,10 @@
 		#endregion
 
 		private Wisej.Web.DataGridView dataGridView1;
+		private Wisej.Web.DataGridViewTextBoxColumn colName;
+		private Wisej.Web.DataGridViewTextBoxColumn colCompany;
+		private Wisej.Web.DataGridViewTextBoxColumn colID;
+		private Wisej.Web.BindingSource personBindingSource;
 	}
 }
 
--- DataboundDGV/Page1.cs
+++ DataboundDGV/Page1.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.Generic;
+using System.ComponentModel;
 using Wisej.Web;
 
 namespace DataboundDGV
@@ -10,17 +11,18 @@ namespace DataboundDGV
 			InitializeComponent();
 		}
 
-		List<Person> peopleList = new List<Person>();
+		BindingList<Person> peopleList = new BindingList<Person>();
 
 		List<Company> companyList = new List<Company>();
 		private void Page1_Load(object sender, System.EventArgs e)
 		{
-			
 			peopleList.Add(new Person() { Name = "John", Company = "ITG", ID = 1 });
 			peopleList.Add(new Person() { Name = "Julie", Company = "ITG", ID = 2 });
 			peopleList.Add(new Person() { Name = "Tom", Company = "Google", ID = 3 });
+
+			this.personBindingSource.DataSource = peopleList;
 			peopleList.Add(new Person() { Name = "Susan", Company = "Google", ID = 4 });
-			dataGridView1.DataSource = peopleList;
+
 		}
 	}
 }
